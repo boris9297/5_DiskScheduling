81fae2ce5a3b4ab048448a7d76af7d417c31cb7e
Boris <boris9297@gmail.com>
2019-03-02 12:31:49 -0800

    Disk class file

--- src/main/java/com/group2/LOOK.java
+++ src/main/java/com/group2/LOOK.java
@@ -1,4 +1,23 @@
 package com.group2;
 
 public class LOOK extends Schedule {
+
+    Disk disk;
+
+    public LOOK (Disk disk) {
+        this.disk = disk;
+    }
+    public void run() {
+        int pre = disk.startPos;
+        int[] sortSchedule = new int[disk.request_array.length];
+
+        for (int i = 0; i < disk.request_array.length; i ++) {
+            int current = disk.request_array[i];
+            totalDistance += Math.abs(current - pre);
+            pre = current;
+            sortSchedule[i] = current;
+        }
+
+        printResult(sortSchedule, totalDistance);
+    }
 }
--- src/main/java/com/group2/Main.java
+++ src/main/java/com/group2/Main.java
@@ -66,29 +66,3 @@ public class Main {
 
 }
 
-class Disk {
-
-    final int size = 5000;
-    final int startPos = 2255;
-    final int previousPos = 1723;
-    final static int[] request_array = {2055, 1175, 2304, 2700, 513, 1680, 256, 1401, 4922, 3692};
-
-    public Disk(){
-        final boolean up = (0 < startPos - previousPos)? true: false ;
-    }
-//
-//    /**
-//     * Initialize all variables
-//     * @param size Cylinder size
-//     * @param position Current header position
-//     * @param numOfReq Number of requests
-//     * @param request_array All requests
-//     */
-//    public Disk(int size, int position,int numOfReq,int[] request_array) {
-//        this.size = size;
-//        startPos = position;
-//        numberOfRequest = numOfReq;
-//        request = new int[numOfReq];
-//        request = request_array;
-//    }
-}
