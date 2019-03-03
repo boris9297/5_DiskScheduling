cc04dc3269ed69dac6cfee38e5a39b712c318459
Lili Yang <yangll124@126.com>
2019-03-02 17:27:53 -0800

    Modify 6 disk scheduling algorithms

--- src/main/java/com/group2/LOOK.java
+++ src/main/java/com/group2/LOOK.java
@@ -1,45 +1,67 @@
 package com.group2;
 
+/*
+LOOK: similar to SCAN but reverses when no more request in current direction disk-scheduling algorithm
+ */
+
 import java.util.Arrays;
 
-public class LOOK extends Schedule {
-
+public class LOOK {
     Disk disk;
+    Schedule schedule;
 
-    public LOOK (Disk disk) {
-        this.disk = disk;
+    public LOOK() {
+        disk = new Disk();
+        schedule = new Schedule();
     }
+
     public void run() {
         int pre = disk.startPos;
         int[] tempSchedule = disk.request_array;
         Arrays.sort(tempSchedule);
-        int[] sortSchedule = new int[disk.request_array.length];
-        totalDistance = 0;
-//        if (disk.up) {
-            int startId = 0;
-            for (int i = 0; i < tempSchedule.length; i ++) {
+        int[] scheduled = new int[disk.request_array.length];
+        int totalDistance = 0;
+        if (disk.up) {
+            int startIdx = 0;
+            for (int i = 0; i < tempSchedule.length; i++) {
                 int current = tempSchedule[i];
                 if (disk.startPos < current) {
-                    sortSchedule[startId] = current;
-                    startId++;
+                    scheduled[startIdx] = current;
+                    startIdx++;
                     totalDistance += Math.abs(current - pre);
                     pre = current;
-
                 }
             }
-
-            for (int i = tempSchedule.length - 1; 0 <= i ; i --) {
+            for (int i = tempSchedule.length - 1; i >= 0; i--) {
                 int current = tempSchedule[i];
                 if (current < disk.startPos) {
-                    sortSchedule[startId] = current;
-                    startId++;
+                    scheduled[startIdx] = current;
+                    startIdx++;
                     totalDistance += Math.abs(current - pre);
                     pre = current;
-
                 }
             }
-//        }
-
-        printResult(sortSchedule, totalDistance);
+        } else {
+            int startIdx = 0;
+            for (int i = tempSchedule.length - 1; i >= 0; i--) {
+                int current = tempSchedule[i];
+                if (current < disk.startPos) {
+                    scheduled[startIdx] = current;
+                    startIdx++;
+                    totalDistance += Math.abs(current - pre);
+                    pre = current;
+                }
+            }
+            for (int i = 0; i < tempSchedule.length; i++) {
+                int current = tempSchedule[i];
+                if (disk.startPos < current) {
+                    scheduled[startIdx] = current;
+                    startIdx++;
+                    totalDistance += Math.abs(current - pre);
+                    pre = current;
+                }
+            }
+        }
+        schedule.printResult(scheduled, totalDistance);
     }
 }
