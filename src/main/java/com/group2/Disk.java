03534e6f5864b3f25b89301bd9804ae1f364e102
Lili Yang <yangll124@126.com>
2019-03-02 17:19:26 -0800

    Modify Main, Disk and Schedule class

--- src/main/java/com/group2/Disk.java
+++ src/main/java/com/group2/Disk.java
@@ -1,29 +1,22 @@
 package com.group2;
 
-class Disk {
+/*
+Customize the Disk class which represents the initialized state of Disk including the total cylinder size, start position,
+previous position and request array. Also, use start position and previous position to determine the current moving direction
+ */
 
-    final int size = 5000;
-    final int startPos = 2255;
-    final int previousPos = 1723;
-    final int[] request_array = {2055, 1175, 2304, 2700, 513, 1680, 256, 1401, 4922, 3692};
-    final boolean up = (0 < startPos - previousPos)? true: false ;
+class Disk {
+    final int size;
+    final int startPos;
+    final int previousPos;
+    final int[] request_array;
+    final boolean up;
 
     public Disk(){
-
+        size = 5000;
+        startPos = 2255;
+        previousPos = 1723;
+        request_array = new int[]{2055, 1175, 2304, 2700, 513, 1680, 256, 1401, 4922, 3692};
+        up = (0 < startPos - previousPos)? true: false;
     }
-//
-//    /**
-//     * Initialize all variables
-//     * @param size Cylinder size
-//     * @param position Current header position
-//     * @param numOfReq Number of requests
-//     * @param request_array All requests
-//     */
-//    public Disk(int size, int position,int numOfReq,int[] request_array) {
-//        this.size = size;
-//        startPos = position;
-//        numberOfRequest = numOfReq;
-//        request = new int[numOfReq];
-//        request = request_array;
-//    }
 }
